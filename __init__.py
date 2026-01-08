76a68089df1c1955273ed0b43c448e610b5ff0f5
Amandinefortat <amandinefortat@gmail.com>
2026-01-08 10:38:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,9 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -17,9 +20,6 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"  
