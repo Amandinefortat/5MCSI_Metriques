14c3c717418c2ed8d266d1ddb555c79a7b172aea
Amandinefortat <amandinefortat@gmail.com>
2026-01-08 10:26:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)
-
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
